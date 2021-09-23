9511a41d0c1a2f899a0f479b812ade90bc1fdabb
SeanOVeryMac <seanovery1@gmail.com>
2021-09-23 17:15:26 -0400

    input boxes added and css updated

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let   now = moment();
 
 
 setInterval(function() {
-    currentDayEl.text(now.format('MMM Do, YYYY [at] h:mm:ss a'))
+    currentDayEl.text(moment().format('MMM Do, YYYY [at] h:mm:ss a'))
 },1000)
 
 function displaySchedule() {
@@ -12,14 +12,14 @@ function displaySchedule() {
         
         let newLi = $('<li>'),
             newSpan = $('<span>'),
-            newSpan2 = $('<span>'),
+            newInput = $('<input>'),
             newBtn = $('<button>');
 
         newLi.attr('class', 'list-group-item d-flex justify-content-between align-items-center')
         newSpan.attr('class', 'inline-time').text(`${moment().startOf('day').add((i + 9), 'h').format('h:mm A')}`)
-        newSpan2.attr('class', 'schedule-item').text('Placeholder')
-        newBtn.attr('class', 'set-btn').text('Placeholder')
-        newLi.append(newSpan, newSpan2, newBtn)
+        newInput.attr('class', 'schedule-item').attr('type', 'text').text('Placeholder')
+        newBtn.attr('class', 'set-btn').text('Save')
+        newLi.append(newSpan, newInput, newBtn)
         listGroupUl.append(newLi)
     }
 }
@@ -28,7 +28,7 @@ function setColor() {
     for (i = 0; i < 9; i++) {
         
         let targetElSpan = listGroupUl.children('li').eq(i).children('span').eq(0).text(),
-            targetElLi = listGroupUl.children('li').eq(i)
+            targetElLi = listGroupUl.children('li').eq(i).children('input')
             targetElSpanMoment = moment(targetElSpan, 'h:mm A');
 
         if (targetElSpanMoment.diff(moment().startOf('hour')) < 0) {
@@ -43,4 +43,3 @@ function setColor() {
 
 displaySchedule();
 setColor();
-console.log(moment(listGroupUl.children('li').children('span').eq(0).text(), 'h:mm A'));
