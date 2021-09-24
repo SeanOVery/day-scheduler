abfe850af102b5d207bcb02d1dd2997fce454772
SeanOVeryMac <seanovery1@gmail.com>
2021-09-24 11:08:53 -0400

    comments, change a few variable names, remove console.logs

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const currentDayEl = $('#currentDay'),
       listGroupUl = $('.list-group');
 let   scheduleStorageArr = ['', '', '', '', '', '', '', '', ''];
-
+// Global variable declaration.
 
 setInterval(function() {
     currentDayEl.text(moment().format('MMM Do, YYYY [at] h:mm:ss a'))
-},1000)
+},1000)// Interval to show time live on page updating every second.
 
 function init() {
-    let storedItems = JSON.parse(localStorage.getItem('schedule'))
+    let storedItems = JSON.parse(localStorage.getItem('schedule'));
+
     if (storedItems !== null) {
         scheduleStorageArr = storedItems
     }
-    console.log(scheduleStorageArr)
+
     displaySchedule();
     setColor();
-}
+} // Function pulls schedule from local storage(if it exists) and displays it.
 
 function displaySchedule() {
     for (i = 0; i < 9; i++) {
@@ -32,34 +33,30 @@ function displaySchedule() {
         newLi.append(newSpan, newInput, newBtn)
         listGroupUl.append(newLi)
     }
-}
+} // Function to display schedule including previously stored items and also set things like the data index on input for easier access later and event listeners on save btns.
 
 function setColor() {
     for (i = 0; i < 9; i++) {
         
-        let targetElSpan = listGroupUl.children('li').eq(i).children('span').eq(0).text(),
-            targetElLi = listGroupUl.children('li').eq(i).children('input')
-            targetElSpanMoment = moment(targetElSpan, 'h:mm A');
+        let targetSpanEl = listGroupUl.children('li').eq(i).children('span').eq(0).text(),
+            targetInputEl = listGroupUl.children('li').eq(i).children('input')
+            targetElSpanMoment = moment(targetSpanEl, 'h:mm A');
 
         if (targetElSpanMoment.diff(moment().startOf('hour')) < 0) {
-            targetElLi.addClass('past')
+            targetInputEl.addClass('past')
         } else if (targetElSpanMoment.diff(moment().startOf('hour')) === 0) {
-            targetElLi.addClass('present')
+            targetInputEl.addClass('present')
         } else {
-            targetElLi.addClass('future')
+            targetInputEl.addClass('future')
         }
     }
-}
+} // Function to set color on input boxes. Sets red input is within current hour, gray if in the past, and green if in the future.
 
 function saveItem(ev) {
     let index = $(ev.target).siblings().eq(-1).attr('data-index'),
-        value = $(ev.target).siblings().eq(-1).val()
+        value = $(ev.target).siblings().eq(-1).val();
     scheduleStorageArr.splice(index, 1, value)
     localStorage.setItem('schedule', JSON.stringify(scheduleStorageArr))
-    console.log(index)
-    console.log(value)
-    console.log(scheduleStorageArr)
+} // Function updates local storage and page with new item added 
 
-}
-
-init();
+init(); // Run on page open
