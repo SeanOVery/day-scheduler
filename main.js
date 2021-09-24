4a31a0e1e778a0579a032fa51683e0a1ef7cdbb4
SeanOVeryMac <seanovery1@gmail.com>
2021-09-24 10:30:40 -0400

    local storage set up and functional

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const currentDayEl = $('#currentDay'),
       listGroupUl = $('.list-group');
-let   now = moment();
+let   scheduleStorageArr = ['', '', '', '', '', '', '', '', ''];
 
 
 setInterval(function() {
     currentDayEl.text(moment().format('MMM Do, YYYY [at] h:mm:ss a'))
 },1000)
+
+function init() {
+    let storedItems = JSON.parse(localStorage.getItem('schedule'))
+    if (storedItems !== null) {
+        scheduleStorageArr = storedItems
+    }
+    console.log(scheduleStorageArr)
+    displaySchedule();
+    setColor();
+}
 
 function displaySchedule() {
     for (i = 0; i < 9; i++) {
@@ -17,8 +27,8 @@ function displaySchedule() {
 
         newLi.attr('class', 'list-group-item d-flex justify-content-between align-items-center')
         newSpan.attr('class', 'inline-time').text(`${moment().startOf('day').add((i + 9), 'h').format('h:mm A')}`)
-        newInput.attr('class', 'schedule-item').attr('type', 'text').text('Placeholder')
-        newBtn.attr('class', 'set-btn').text('Save')
+        newInput.attr('class', 'schedule-item').attr('type', 'text').attr('data-index', i).val(scheduleStorageArr[i])
+        newBtn.attr('class', 'set-btn').text('Save').on('click', saveItem)
         newLi.append(newSpan, newInput, newBtn)
         listGroupUl.append(newLi)
     }
@@ -41,5 +51,15 @@ function setColor() {
     }
 }
 
-displaySchedule();
-setColor();
+function saveItem(ev) {
+    let index = $(ev.target).siblings().eq(-1).attr('data-index'),
+        value = $(ev.target).siblings().eq(-1).val()
+    scheduleStorageArr.splice(index, 1, value)
+    localStorage.setItem('schedule', JSON.stringify(scheduleStorageArr))
+    console.log(index)
+    console.log(value)
+    console.log(scheduleStorageArr)
+
+}
+
+init();
